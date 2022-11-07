3a3934f79544ab74258281ec70a37a0c380f1bf7
zhenyaFlower <onlymirage@live.ru>
2022-11-07 20:25:15 +0300

    First commit

---
+++ app/src/main/java/com/zhenya_flower/firstlesson_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zhenya_flower.firstlesson_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
